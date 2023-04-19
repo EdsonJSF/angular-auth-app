39d30dc0fa0d3af24cdac2bcfaa03e1d3be7ea68
Edsonjsf <edsonjsf1990@gmail.com>
2023-04-19 13:32:55 -0400

    Pantalla protegida

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -13,10 +14,11 @@ export class RegisterComponent {
     pass: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   registro() {
     console.log(this.miFormulario.value);
-    console.log(this.miFormulario.valid);
+
+    this.router.navigateByUrl('dashboard');
   }
 }
